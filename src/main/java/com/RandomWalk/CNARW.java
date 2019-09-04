653c61f590e32427b35d12dc3468c128f10d64b1
hqf1996 <378938941@qq.com>
2019-09-04 18:47:32 +0800

    CNARW修改

--- src/main/java/com/RandomWalk/CNARW.java
+++ src/main/java/com/RandomWalk/CNARW.java
@@ -14,25 +14,77 @@ public class CNARW extends Graph<NodeCNARW, Double>{
     List<String> AllPath = new ArrayList<>();      //所有的路径
     Map<String, Integer> PathBefore = new LinkedHashMap<>();   //在稳态之间对路径的统计
     Map<String, Integer> PathAfter = new LinkedHashMap<>();    //在稳态之后对路径的统计
+    double [][]P_value;
+    int nodeNum;
     /**
      * 初始化图
      * @return
      */
     private void initGraph(){
         Graph<NodeCNARW, Double> graph = new Graph<>(false);
-        String []nodes_names = {"u" ,"a", "b", "c", "d", "v", "e", "f", "g", "h", "add1", "add2", "add3", "add4", "add5", "add6",
-                "add7", "add8", "add9", "add10", "add11", "add12", "add13", "add14", "add15"};
-        double []nodes_values = {1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        NodeCNARW []nodes = new NodeCNARW[25];
+//        String []nodes_names = {"u" ,"a", "b", "c", "d", "v", "e", "f", "g", "h", "add1", "add2", "add3", "add4", "add5", "add6",
+//                "add7", "add8", "add9", "add10", "add11", "add12", "add13", "add14", "add15"};
+//        double []nodes_values = {1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        String []nodes_names = {"u" ,"a", "b", "c", "d", "v", "e", "f", "g", "h"};
+        double []nodes_values = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        NodeCNARW []nodes = new NodeCNARW[nodes_names.length];
         for (int i = 0 ; i < nodes_names.length ; ++i){
             nodes[i] = new NodeCNARW(nodes_names[i], nodes_values[i]);
         }
-        NodeCNARW [][]edges = {
-                {new NodeCNARW("u", 1.0), new NodeCNARW("a", 0)},
-                {new NodeCNARW("u", 1.0), new NodeCNARW("b", 0)},
-                {new NodeCNARW("u", 1.0), new NodeCNARW("c", 0)},
-                {new NodeCNARW("u", 1.0), new NodeCNARW("d", 0)},
-                {new NodeCNARW("u", 1.0), new NodeCNARW("v", 0)},
+//        NodeCNARW [][]edges = {
+//                {new NodeCNARW("u", 1.0), new NodeCNARW("a", 0)},
+//                {new NodeCNARW("u", 1.0), new NodeCNARW("b", 0)},
+//                {new NodeCNARW("u", 1.0), new NodeCNARW("c", 0)},
+//                {new NodeCNARW("u", 1.0), new NodeCNARW("d", 0)},
+//                {new NodeCNARW("u", 1.0), new NodeCNARW("v", 0)},
+//                {new NodeCNARW("a", 0), new NodeCNARW("b", 0)},
+//                {new NodeCNARW("b", 0), new NodeCNARW("d", 0)},
+//                {new NodeCNARW("b", 0), new NodeCNARW("e", 0)},
+//                {new NodeCNARW("d", 0), new NodeCNARW("c", 0)},
+//                {new NodeCNARW("c", 0), new NodeCNARW("f", 0)},
+//                {new NodeCNARW("c", 0), new NodeCNARW("h", 0)},
+//                {new NodeCNARW("v", 0), new NodeCNARW("g", 0)},
+//                {new NodeCNARW("v", 0), new NodeCNARW("e", 0)},
+//                {new NodeCNARW("v", 0), new NodeCNARW("f", 0)},
+//                {new NodeCNARW("e", 0), new NodeCNARW("g", 0)},
+//                {new NodeCNARW("g", 0), new NodeCNARW("h", 0)},
+//                {new NodeCNARW("add1", 0), new NodeCNARW("a", 0)},
+//                {new NodeCNARW("add1", 0), new NodeCNARW("g", 0)},
+//                {new NodeCNARW("add1", 0), new NodeCNARW("add2", 0)},
+//                {new NodeCNARW("add2", 0), new NodeCNARW("v", 0)},
+//                {new NodeCNARW("d", 0), new NodeCNARW("add3", 0)},
+//                {new NodeCNARW("add3", 0), new NodeCNARW("h", 0)},
+//                {new NodeCNARW("g", 0), new NodeCNARW("add4", 0)},
+//                {new NodeCNARW("add4", 0), new NodeCNARW("add5", 0)},
+//                {new NodeCNARW("add5", 0), new NodeCNARW("add6", 0)},
+//                {new NodeCNARW("h", 0), new NodeCNARW("add6", 0)},
+//                {new NodeCNARW("add6", 0), new NodeCNARW("add7", 0)},
+//                {new NodeCNARW("add7", 0), new NodeCNARW("add8", 0)},
+//                {new NodeCNARW("add6", 0), new NodeCNARW("add9", 0)},
+//                {new NodeCNARW("h", 0), new NodeCNARW("add7", 0)},
+//                {new NodeCNARW("add4", 0), new NodeCNARW("add9", 0)},
+//                {new NodeCNARW("add8", 0), new NodeCNARW("add9", 0)},
+//                {new NodeCNARW("h", 0), new NodeCNARW("add10", 0)},
+//                {new NodeCNARW("add3", 0), new NodeCNARW("add10", 0)},
+//                {new NodeCNARW("add10", 0), new NodeCNARW("add7", 0)},
+//                {new NodeCNARW("add10", 0), new NodeCNARW("add11", 0)},
+//                {new NodeCNARW("add11", 0), new NodeCNARW("add7", 0)},
+//                {new NodeCNARW("add11", 0), new NodeCNARW("add12", 0)},
+//                {new NodeCNARW("add12", 0), new NodeCNARW("add9", 0)},
+//                {new NodeCNARW("add10", 0), new NodeCNARW("add13", 0)},
+//                {new NodeCNARW("add13", 0), new NodeCNARW("add14", 0)},
+//                {new NodeCNARW("add5", 0), new NodeCNARW("add15", 0)},
+//                {new NodeCNARW("add15", 0), new NodeCNARW("add9", 0)},
+//                {new NodeCNARW("add14", 0), new NodeCNARW("add12", 0)},
+//                {new NodeCNARW("add14", 0), new NodeCNARW("add8", 0)}
+//                };
+//        Double[] predicates = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+//                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0};
+        NodeCNARW [][]edges = {{new NodeCNARW("u", 1), new NodeCNARW("a", 0)},
+                {new NodeCNARW("u", 1), new NodeCNARW("b", 0)},
+                {new NodeCNARW("u", 1), new NodeCNARW("c", 0)},
+                {new NodeCNARW("u", 1), new NodeCNARW("d", 0)},
+                {new NodeCNARW("u", 1), new NodeCNARW("v", 0)},
                 {new NodeCNARW("a", 0), new NodeCNARW("b", 0)},
                 {new NodeCNARW("b", 0), new NodeCNARW("d", 0)},
                 {new NodeCNARW("b", 0), new NodeCNARW("e", 0)},
@@ -43,112 +95,31 @@ public class CNARW extends Graph<NodeCNARW, Double>{
                 {new NodeCNARW("v", 0), new NodeCNARW("e", 0)},
                 {new NodeCNARW("v", 0), new NodeCNARW("f", 0)},
                 {new NodeCNARW("e", 0), new NodeCNARW("g", 0)},
-                {new NodeCNARW("g", 0), new NodeCNARW("h", 0)},
-                {new NodeCNARW("add1", 0), new NodeCNARW("a", 0)},
-                {new NodeCNARW("add1", 0), new NodeCNARW("g", 0)},
-                {new NodeCNARW("add1", 0), new NodeCNARW("add2", 0)},
-                {new NodeCNARW("add2", 0), new NodeCNARW("v", 0)},
-                {new NodeCNARW("d", 0), new NodeCNARW("add3", 0)},
-                {new NodeCNARW("add3", 0), new NodeCNARW("h", 0)},
-                {new NodeCNARW("g", 0), new NodeCNARW("add4", 0)},
-                {new NodeCNARW("add4", 0), new NodeCNARW("add5", 0)},
-                {new NodeCNARW("add5", 0), new NodeCNARW("add6", 0)},
-                {new NodeCNARW("h", 0), new NodeCNARW("add6", 0)},
-                {new NodeCNARW("add6", 0), new NodeCNARW("add7", 0)},
-                {new NodeCNARW("add7", 0), new NodeCNARW("add8", 0)},
-                {new NodeCNARW("add6", 0), new NodeCNARW("add9", 0)},
-                {new NodeCNARW("h", 0), new NodeCNARW("add7", 0)},
-                {new NodeCNARW("add4", 0), new NodeCNARW("add9", 0)},
-                {new NodeCNARW("add8", 0), new NodeCNARW("add9", 0)},
-                {new NodeCNARW("h", 0), new NodeCNARW("add10", 0)},
-                {new NodeCNARW("add3", 0), new NodeCNARW("add10", 0)},
-                {new NodeCNARW("add10", 0), new NodeCNARW("add7", 0)},
-                {new NodeCNARW("add10", 0), new NodeCNARW("add11", 0)},
-                {new NodeCNARW("add11", 0), new NodeCNARW("add7", 0)},
-                {new NodeCNARW("add11", 0), new NodeCNARW("add12", 0)},
-                {new NodeCNARW("add12", 0), new NodeCNARW("add9", 0)},
-                {new NodeCNARW("add10", 0), new NodeCNARW("add13", 0)},
-                {new NodeCNARW("add13", 0), new NodeCNARW("add14", 0)},
-                {new NodeCNARW("add5", 0), new NodeCNARW("add15", 0)},
-                {new NodeCNARW("add15", 0), new NodeCNARW("add9", 0)},
-                {new NodeCNARW("add14", 0), new NodeCNARW("add12", 0)},
-                {new NodeCNARW("add14", 0), new NodeCNARW("add8", 0)}
-                };
-        Double[] predicates = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0};
-//        NodeCNARW [][]edges = {{new NodeCNARW("u", 0.1), new NodeCNARW("a", 0.1)},
-//                {new NodeCNARW("u", 0.1), new NodeCNARW("b", 0.1)},
-//                {new NodeCNARW("u", 0.1), new NodeCNARW("c", 0.1)},
-//                {new NodeCNARW("u", 0.1), new NodeCNARW("d", 0.1)},
-//                {new NodeCNARW("u", 0.1), new NodeCNARW("v", 0.1)},
-//                {new NodeCNARW("a", 0.1), new NodeCNARW("b", 0.1)},
-//                {new NodeCNARW("b", 0.1), new NodeCNARW("d", 0.1)},
-//                {new NodeCNARW("b", 0.1), new NodeCNARW("e", 0.1)},
-//                {new NodeCNARW("d", 0.1), new NodeCNARW("c", 0.1)},
-//                {new NodeCNARW("c", 0.1), new NodeCNARW("f", 0.1)},
-//                {new NodeCNARW("c", 0.1), new NodeCNARW("h", 0.1)},
-//                {new NodeCNARW("v", 0.1), new NodeCNARW("g", 0.1)},
-//                {new NodeCNARW("v", 0.1), new NodeCNARW("e", 0.1)},
-//                {new NodeCNARW("v", 0.1), new NodeCNARW("f", 0.1)},
-//                {new NodeCNARW("e", 0.1), new NodeCNARW("g", 0.1)},
-//                {new NodeCNARW("g", 0.1), new NodeCNARW("h", 0.1)}};
+                {new NodeCNARW("g", 0), new NodeCNARW("h", 0)}};
+        Double[] predicates = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         graph.createGraph(nodes, edges, predicates);
         this.graph = graph;
+        nodeNum = nodes_names.length;
+        P_value = new double[nodeNum][nodeNum];
+        long startTime = System.currentTimeMillis();
+        CalTransitionMatrix(nodeNum);
+        System.out.println("计算转移矩阵的时间为：" + String.valueOf(System.currentTimeMillis()-startTime) + "ms");
     }
-
 
     /**
      * 计算出转移矩阵
-     * @param start 从哪个点开始做一次bfs
+     * @param nodesNum
      */
-    private void CalTransitionMatrix(int start){
-        Set<Integer> hasVisited = new HashSet<>(); //已经遍历过的点
-        Set<Integer> currentVisited = new HashSet<>(); //正在遍历队列中 还没有遍历的点
-        currentVisited.add(0);  //从u点开始
-        while (!currentVisited.isEmpty()){
-            Set<Integer> NextNode = new HashSet<>();
-            for (Integer cur : currentVisited){
-                hasVisited.add(cur);
-                Iterator<Graph.ArcNode> iterator = graph.iterator(cur);
-                List<Integer> nextAdjvex = new ArrayList<>();
-                // 获取当前节点的所有临界点的adjvex
-                while (iterator.hasNext()){
-                    ArcNode arcNode = iterator.next();
-                    int adjvex = arcNode.adjvex;
-                    nextAdjvex.add(adjvex);
-                    if (!hasVisited.contains(adjvex)){
-                        NextNode.add(adjvex);
-                    }
-                }
-                //计算当前节点所有边的q值和，可以重用，就不用重复计算
-                double q_sum = 0.0;
-                for (Integer each : nextAdjvex){
-                    int[] result = getPublicNodeNumAndDeg(cur, each);
-                    int C = result[0];  //公共点数
-                    int minDeg = result[1];  //度的最小值
-                    q_sum += (1-1.0*C/minDeg);
-                }
-                // 计算quv
-                for (Integer each : nextAdjvex){
-                    int[] result = getPublicNodeNumAndDeg(cur, each);
-                    int C = result[0];  //公共点数
-                    int minDeg = result[1];  //度的最小值
-                    double q_uv = 1-1.0*C/minDeg;
-                    double p_uv_ba = 1.0*q_uv/getDegree(cur);
-                    double p_uu_ba = 1-(1.0/getDegree(cur))*(q_sum);
-                    double P_uv = p_uv_ba/(1-p_uu_ba);
-                    VexNode<NodeCNARW> nodeCNARWVexNode1 = graph.GetDataByIndex(cur);
-                    VexNode<NodeCNARW> nodeCNARWVexNode2 = graph.GetDataByIndex(each);
-                    System.out.println(nodeCNARWVexNode1.data.node_name+"->"+nodeCNARWVexNode2.data.node_name + ":" + P_uv);
-                }
+    private void CalTransitionMatrix(int nodesNum){
+        for (int i = 0 ; i < nodesNum ; ++i){
+            for (int j = 0 ; j < nodesNum ; ++j){
+                P_value[i][j] = this.getP_uvValue(i, j);
             }
-            System.out.println("___________________");
-            currentVisited = NextNode;
         }
     }
 
     /**
-     * 返回两个点的P值
+     * 通过直接计算，返回两个点的P值
      * @param start
      * @param end
      * @return
@@ -188,6 +159,16 @@ public class CNARW extends Graph<NodeCNARW, Double>{
         double P_uv = p_uv_ba / (1 - p_uu_ba);
 //        System.out.println(nodeCNARWVexNode1.data.node_name + "->" + nodeCNARWVexNode2.data.node_name + ":" + P_uv);
         return P_uv;
+    }
+
+    /**
+     * 通过先前就计算好的转移矩阵来取得当前两个点之间的P值
+     * @param start
+     * @param end
+     * @return
+     */
+    private double getP_uvValueByMatrix(int start, int end){
+        return P_value[start][end];
     }
 
     /**
@@ -290,36 +271,70 @@ public class CNARW extends Graph<NodeCNARW, Double>{
         Set<Integer> NodeCollect = new HashSet<>();  //采样得到的点的序号
         int i = 0;
         long starttime = System.currentTimeMillis();
-        while (i < 1000){
+        while (i < 10000){
             vectorBefore = getVector();
             System.out.println(vectorBefore.toString());
             System.out.println("走了" + (i+1) + "条路");
             System.out.println(System.currentTimeMillis()-starttime + "ms");
             //随机选择下一跳节点，用拒绝的方式
             int nextNodeIndex = nextNode(currentNodeIndex);
-            //更新下一跳节点的向量概率
+            //计算下一跳节点的向量概率
             VexNode<NodeCNARW> currentNode = graph.GetDataByIndex(currentNodeIndex);
             VexNode<NodeCNARW> nextNode = graph.GetDataByIndex(nextNodeIndex);
             String path = currentNode.data.getNode_name()+"->"+nextNode.data.getNode_name();
             AllPath.add(path);
             System.out.println(path);
-            nextNode.data.setNode_values(getSumToNode(nextNodeIndex));
-            vectorAfter = getVector();
-            currentNodeIndex = nextNodeIndex;
-            i++;
-            // 判断是否达到稳态，若达到稳态，则开始采样。达到想要采样的个数后停止随机游走
-            if (IsBanance(vectorBefore, vectorAfter)){
-                isBanance = true;
-            }
-            if (isBanance){
-                System.out.println("稳态 " + path);
-                if (NodeCollect.size() < k){
-                    System.out.println("加入点" + currentNodeIndex);
-                    NodeCollect.add(currentNodeIndex);
-                }else {
-                    break;
+            /**----------------------第一种处理方式，符合矩阵乘法的逻辑-----------------------*/
+            // 更新下一跳节点，暂时记录下来
+            double nextNodeValue = getSumToNode(nextNodeIndex);
+            //根据当前的还未更新新点的向量去更新其他图上所有点，暂时记录下来
+            double[] tempNodeValue = new double[P_value.length];
+            for (int w = 0 ; w < this.nodeNum ; ++w){
+                if (w != nextNodeIndex){
+                    tempNodeValue[w] = getSumToNode(w);
                 }
             }
+            // 更新下一跳以及所有点
+            for (int w = 0 ; w < this.nodeNum ; ++w){
+                ((NodeCNARW)graph.GetDataByIndex(w).data).setNode_values(tempNodeValue[w]);
+            }
+            nextNode.data.setNode_values(nextNodeValue);
+            /**-------------------第二种处理方式，用新的点去更新其他的向量-----------------------*/
+            /**ps:这种方法有问题，最终达到稳态的时候，所有点的值相加并不等于1，因为和矩阵的乘法没有对应上的原因*/
+//            // 更新下一跳节点，直接更新
+//            double nextNodeValue = getSumToNode(nextNodeIndex);
+//            nextNode.data.setNode_values(nextNodeValue);
+//            //根据当前的还未更新新点的向量去更新其他图上所有点，暂时记录下来
+//            double[] tempNodeValue = new double[P_value.length];
+//            for (int w = 0 ; w < this.nodeNum ; ++w){
+//                if (w != nextNodeIndex){
+//                    tempNodeValue[w] = getSumToNode(w);
+//                }
+//            }
+//            // 更新除下一跳以外的所有点
+//            for (int w = 0 ; w < this.nodeNum ; ++w){
+//                if (w != nextNodeIndex){
+//                    ((NodeCNARW)graph.GetDataByIndex(w).data).setNode_values(tempNodeValue[w]);
+//                }
+//            }
+
+            vectorAfter = getVector();
+            System.out.println(vectorAfter.toString());
+            currentNodeIndex = nextNodeIndex;
+            i++;
+//            // 判断是否达到稳态，若达到稳态，则开始采样。达到想要采样的个数后停止随机游走
+//            if (IsBanance(vectorBefore, vectorAfter)){
+//                isBanance = true;
+//            }
+//            if (isBanance){
+//                System.out.println("稳态 " + path);
+//                if (NodeCollect.size() < k){
+//                    System.out.println("加入点" + currentNodeIndex);
+//                    NodeCollect.add(currentNodeIndex);
+//                }else {
+//                    break;
+//                }
+//            }
         }
         // 开始得到估计值
         double sumf = 0.0;
@@ -331,7 +346,6 @@ public class CNARW extends Graph<NodeCNARW, Double>{
             sum+=weight;
         }
         System.out.println("估计值为：" + sumf/sum);
-
     }
 
     /**
@@ -344,7 +358,8 @@ public class CNARW extends Graph<NodeCNARW, Double>{
         double sum = 0.0;
         while (iterator.hasNext()){
             ArcNode arcNode = iterator.next();
-            double p_uvValue = getP_uvValue(arcNode.adjvex, nodeIndex);
+//            double p_uvValue = getP_uvValue(arcNode.adjvex, nodeIndex);
+            double p_uvValue = getP_uvValueByMatrix(arcNode.adjvex, nodeIndex);
             VexNode<NodeCNARW> currentNode = graph.GetDataByIndex(arcNode.adjvex);
             sum+=(p_uvValue*currentNode.data.getNode_values());
         }
@@ -465,10 +480,7 @@ public class CNARW extends Graph<NodeCNARW, Double>{
 //        }
 //        System.out.println(cn.nextNode(0));
         // 从第几个点开始随机游走，在达到稳态之后取几个样本点
-        cn.RandomWalk(0, 15);
-//        Scanner in = new Scanner(System.in);
-//        int times = in.nextInt();
-//        cn.PathStatistics(times);
+        cn.RandomWalk(0, 6);
         System.out.println("度的真实值为：" + cn.getTrueValue());
     }
 }
